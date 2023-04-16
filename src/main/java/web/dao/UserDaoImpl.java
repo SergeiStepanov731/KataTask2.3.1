a1773d06b9ebd5696528b203259060e10f3552d6
SergeiStepanov731 <iffymailone@gmail.com>
2023-04-16 11:01:45 +0300

    update

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,14 +1,11 @@
 package web.dao;
 
-import org.hibernate.Session;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-import web.entity.User;
 
+import org.springframework.stereotype.Repository;
+import web.entity.User;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+
 import javax.persistence.TypedQuery;
 import java.util.List;
 @Repository
