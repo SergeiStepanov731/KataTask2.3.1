10d34375d6561fd112a4f360cd605e9ff304ab3c
SergeiStepanov731 <iffymailone@gmail.com>
2023-04-16 11:02:50 +0300

    update

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -14,28 +14,31 @@ public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
-        this.userService=userService;
+        this.userService = userService;
     }
 
 
     @RequestMapping("/")
-public String showAllUsers(Model model){
-        List<User>allUsers=userService.getAllUsers();
-        model.addAttribute("allUsers",allUsers);
-    return "allUsers";
-}
-@RequestMapping(value="/AddNewUser")
-public String addNewUser(Model model){
-        User user=new User();
-        model.addAttribute("user",user);
-
-    return "AddNewUser";
+    public String showAllUsers(Model model) {
+        List<User> allUsers = userService.getAllUsers();
+        model.addAttribute("allUsers", allUsers);
+        return "allUsers";
     }
+
+    @RequestMapping(value = "/AddNewUser")
+    public String addNewUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+
+        return "AddNewUser";
+    }
+
     @RequestMapping("saveUser")
-    public String saveUser(@ModelAttribute("user") User user){
+    public String saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
         return "redirect:/";
     }
+
     @GetMapping("/edit/{id}")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("user", userService.getUser(id));
@@ -47,8 +50,9 @@ public String addNewUser(Model model){
         userService.update(id, user);
         return "redirect:/";
     }
+
     @DeleteMapping("/delete/{id}")
-    public String delete(@PathVariable("id")int id){
+    public String delete(@PathVariable("id") int id) {
         userService.delete(id);
         return "redirect:/";
     }
